24455d827934d54a942ca66f11c6bec19964dc73
peijyunshih <peijyun.shih@gmail.com>
2016-08-03 15:38:27 +0800

    將 test.js 的測試修正，執行 npm test -dev，結果顯示 1 passing，通過測試。

--- test/test.js
+++ test/test.js
@@ -3,8 +3,13 @@ var assert = require("assert");
 describe('Array', function(){
 	describe('#indexOf()', function(){
 		it('should return -1 when the value is not present', function(){
-			//一筆錯誤的測試 
-			assert.equal(1, [1,2,3].indexOf(0));
+			
+			// //一筆錯誤的測試 
+			// //結果顯示 1 failing，測試沒通過，因為 [1,2,3].indexOf(0) 回傳的值不等於 -1
+			// assert.equal(1, [1,2,3].indexOf(0));
+
+			//一筆正確的測試 
+			assert.equal(-1, [1,2,3].indexOf(0));
 		})
 	})
 })
